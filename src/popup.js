af6f3a6884cbb7e488f4b925d84484c7d1cad759
emma jeong <80943394+sejinniemma@users.noreply.github.com>
2021-07-16 21:49:23 +0900

    refectoring : popup

---
+++ src/popup.js
@@ -0,0 +1,27 @@
+'use strict'
+
+export default class PopUp {
+    constructor() {
+        this.popUp = document.querySelector('.pop-up');
+        this.popUpText = document.querySelector('.pop-up__message');
+        this.popUpRefresh = document.querySelector('.pop-up__refresh');
+        this.popUpRefresh.addEventListener('click',()=>{
+            this.onclick && this.onclick();
+            this.hide();
+            
+        })
+    }
+    
+    setClickListener(onclick){
+        this.onclick = onclick;
+    }
+
+    showWithText(text){
+        this.popUp.classList.remove('pop-up--hide');
+        this.popUpText.innerHTML = text;
+    }
+
+    hide(){
+        this.popUp.classList.add('pop-up--hide');
+    }
+}
